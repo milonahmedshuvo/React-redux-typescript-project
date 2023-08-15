6aeaaedc3d1ce2ad320bf250899610adf41928c4
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-08-15 13:04:45 +0600

    react-redux-typescript-setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useSelector } from "react-redux"
+import { RootState } from "./Redux/Store"
+
+function App() {
+ 
+    const {count} = useSelector( (state:RootState) => state.counter)
+
+  return (
+    <>
+      <h1 className="text-blue-500">Redux explore</h1>
+
+      <button type="button"  className="bg-purple-500 py-1 text-white px-2 rounded">Increment</button>
+      <div>  {count}  </div>
+      <button type="button"  className="bg-blue-600 py-1 text-white px-2 rounded">Decrement</button>
+    </>
+  )
+}
+
+export default App
